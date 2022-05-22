2cc3946205be6655347e4f6d7a3ea6b9a8e6c34d
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-05-22 11:32:16 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## WhatsApp Bussiness API Integration
+
+Work with WhatsApp Bussiness Cloud API from your Frappe site
+
+#### License
+
+MIT
---
+++ waba_integration/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "WhatsApp Bussiness API Integration",
+			"type": "module",
+			"label": _("WhatsApp Bussiness API Integration")
+		}
+	]
