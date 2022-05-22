7bf1217b605b7ca1d1024457e3268a6bb05af2cf
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-05-22 18:35:22 +0530

    chore: Webhook verification

--- waba_integration/api/webhook.py
+++ waba_integration/api/webhook.py
@@ -1,9 +1,12 @@
 import frappe
 
+from werkzeug.wrappers import Response
+
 @frappe.whitelist(allow_guest=True)
 def handle():
     if (frappe.request.method == "GET"):
-        return frappe.request.data
+        data = frappe.request.get_data()
+        return Response(data, status=200)
 
     print("Webhook reached.")
     print(frappe.request.data)
