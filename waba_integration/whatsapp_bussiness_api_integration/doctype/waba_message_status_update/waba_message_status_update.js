31d664360afc4461be0330e5ef9becf2c23e955b
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-05-22 13:08:22 +0530

    feat: More cool DocTypes!

---
+++ waba_integration/whatsapp_bussiness_api_integration/doctype/waba_message_status_update/waba_message_status_update.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WABA Message Status Update', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ waba_integration/whatsapp_bussiness_api_integration/doctype/waba_whatsapp_message/waba_whatsapp_message.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WABA WhatsApp Message', {
+	// refresh: function(frm) {
+
+	// }
+});
