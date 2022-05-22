41f08db8cc82eb3a101a62578424891aec82303e
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-05-22 19:14:18 +0530

    chore: Verify token and fulfill challenge

--- waba_integration/api/webhook.py
+++ waba_integration/api/webhook.py
@@ -2,11 +2,20 @@ import frappe
 
 from werkzeug.wrappers import Response
 
+
 @frappe.whitelist(allow_guest=True)
 def handle():
-    if (frappe.request.method == "GET"):
-        data = frappe.request.get_data()
-        return Response(data, status=200)
+	if frappe.request.method == "GET":
+		return verify_token_and_fulfill_challenge()
 
-    print("Webhook reached.")
-    print(frappe.request.data)
+	# TODO: Handle Post Request
+
+
+def verify_token_and_fulfill_challenge():
+	meta_challenge = frappe.form_dict.get("hub.challenge")
+	expected_token = frappe.db.get_single_value("WABA Settings", "webhook_verify_token")
+
+	if frappe.form_dict.get("hub.verify_token") != expected_token:
+		frappe.throw("Verify token does not match")
+
+	return Response(meta_challenge, status=200)
